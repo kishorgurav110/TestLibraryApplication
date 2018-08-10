81d5982b6e1f9e417d38ae90cc940f5f6c8b3910
kishor <kishorg.express110@gmail.com>
2018-08-10 17:28:53 +0530

    Added Multiply

--- testkishorglibrary/src/main/java/com/example/quicsolv/testkishorglibrary/CustomClassTestKishor.java
+++ testkishorglibrary/src/main/java/com/example/quicsolv/testkishorglibrary/CustomClassTestKishor.java
@@ -2,12 +2,18 @@ package com.example.quicsolv.testkishorglibrary;
 
 public class CustomClassTestKishor {
 
+    // Addition
     public static int Add(int a, int b) {
         return a + b;
     }
 
-    //dkhfjhd
+    //Subtraction
     public static int Minus(int a, int b) {
         return a - b;
+    }
+
+    //Multiplication
+    public static int Multiply(int a, int b) {
+        return a * b;
     }
 }
